b553c1e551abb248bc49e1524f35c30cba531d05
Asad Zia <asaad_zia@hotmail.com>
2016-03-27 19:32:06 +0200

    Update VigenereBreaker.java

--- Java Implementation/src/VigenereBreaker.java
+++ Java Implementation/src/VigenereBreaker.java
@@ -4,6 +4,12 @@
  * @Version 1.0
  */
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class VigenereBreaker 
 {
 	/**
@@ -34,23 +40,89 @@ public class VigenereBreaker
         return cc.toString();
     }
 
-    
+    /**
+     * This method makes use of the CaesarCracker class, as well as
+     * the sliceString method, to find the shift for each index in the key. 
+     * You should fill in the key (which is an array of integers) and return it.
+     * @param encrypted
+     * @param klength
+     * @param mostCommon
+     * @return
+     */
     public int[] tryKeyLength(String encrypted, int klength, char mostCommon) 
     {
-        int[] key = new int[klength];
-        
-        return key;
-    }
-
-    public void breakVigenere () 
-    {
-        //WRITE YOUR CODE HERE
+       int[] key = new int[klength];
+       CaesarCracker cc = new CaesarCracker(mostCommon);
+       
+       int i = 0;
+       
+       while (i < klength)
+       {
+    	   String myString = sliceString(encrypted, i, klength);
+    	   int result = cc.getKey(myString);
+    	   key[i] = result;
+    	   ++i;
+       }
+       return key;
     }
     
+    /**
+     * The function for reading the string content from the file.
+     * @param filename
+     * @return
+     */
+    public String readString (String filename)
+    {
+    	try 
+    	{
+    		StringBuilder sb = new StringBuilder();
+			@SuppressWarnings("resource")
+			BufferedReader encrypted = new BufferedReader(new InputStreamReader(new FileInputStream(filename)));
+			String line;
+			
+			try 
+			{
+				while ((line = encrypted.readLine()) != null)
+				{
+					sb.append(line);
+					sb.append("\n");
+				}
+			} 
+			catch (IOException e) 
+			{
+
+				e.printStackTrace();
+			}
+			
+			return sb.toString();
+		}
+    	catch (FileNotFoundException e) 
+    	{
+
+			e.printStackTrace();
+		}
+		return null;
+    }
+
+    /**
+     * The main method for breaking the Vigenere cipher
+     */
+    public void breakVigenere (String fileName, int klength, char mostCommon) 
+    {
+    	VigenereBreaker cc = new VigenereBreaker();
+    	String encrypted = cc.readString(fileName);
+    	VigenereCipher myCipher = new VigenereCipher (cc.tryKeyLength(encrypted, klength, mostCommon));
+    	String result = myCipher.decrypt(encrypted);
+    	System.out.println(result);
+    }
+    
+    /**
+     * The driver method for the class
+     * @param args
+     */
     public static void main (String args [])
     {
     	VigenereBreaker cc = new VigenereBreaker();
-    	System.out.println(cc.sliceString("abcdefghijklm", 0, 3));
-    }
-    
+    	cc.breakVigenere("messages/athens_keyflute.txt", 5, 'e');
+    }   
 }
